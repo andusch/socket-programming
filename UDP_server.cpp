155e57e826001a66967259a8ee20bc9f131aa32f
Ioan-Alexandru Scheusan <andu.scheusan@gmail.com>
2026-05-16 17:37:56 +0300

    feat: added UDP server

---
+++ UDP_server.cpp
@@ -0,0 +1,63 @@
+#include <cstring>
+#include <iostream>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+const short PORT = 8080;
+
+int main() {
+
+  int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (serverSocket < 0) {
+    perror("socket failed");
+    return 1;
+  }
+
+  int opt = 1;
+  setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  sockaddr_in serverAddress{};
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(PORT);
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(serverSocket, (struct sockaddr *)&serverAddress,
+           sizeof(serverAddress)) < 0) {
+    perror("bind failed");
+    close(serverSocket);
+    return 1;
+  }
+
+  if (listen(serverSocket, 5) < 0) {
+    perror("listen failed");
+    return 1;
+  }
+
+  std::cout << "Server listening on port " << PORT << "...\n";
+
+  int clientSocket = accept(serverSocket, nullptr, nullptr);
+  if (clientSocket < 0) {
+    perror("accept failed");
+    close(serverSocket);
+    return 1;
+  }
+
+  char buffer[1024] = {0};
+  ssize_t bytes = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+  if (bytes > 0) {
+    std::cout << "Message from client: " << buffer << '\n';
+  } else if (bytes == 0) {
+    std::cout << "Client disconnected\n";
+  } else {
+    perror("recv failed");
+  }
+
+  close(clientSocket);
+  close(serverSocket);
+
+  return 0;
+}
